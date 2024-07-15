3bc76272e87f7e7f49f553de8023d834bb37b259
Pablo Reyes <p.reyes1704@gmail.com>
2024-07-15 02:53:52 -0600

    Se elimina propiedad final

--- src/main/java/nova/unidad/domain/model/Contacto.java
+++ src/main/java/nova/unidad/domain/model/Contacto.java
@@ -1,18 +1,18 @@
 package nova.unidad.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
 
+
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Contacto implements Serializable {
-    private final Integer id;
-    private final String telefono;
-    private final String direccion;
-    private final Boolean estado;
+    private Integer id;
+    private String telefono;
+    private String direccion;
+    private Boolean estado;
 }
