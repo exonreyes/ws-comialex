26cc9cefe471e30bc8b91bc1d47ce75d63f8dd8c
Pablo Reyes <p.reyes1704@gmail.com>
2024-07-20 04:16:17 -0600

    Se agrega soporte de constructor

--- src/main/java/nova/unidad/domain/model/Unidad.java
+++ src/main/java/nova/unidad/domain/model/Unidad.java
@@ -2,12 +2,14 @@ package nova.unidad.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import nova.horario.domain.model.Horario;
 
 import java.io.Serializable;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Unidad implements Serializable {
     private Integer id;
@@ -15,4 +17,10 @@ public class Unidad implements Serializable {
     private String nombre;
     private Contacto contacto;
     private List<Horario> horarios;
+
+    public Unidad(Integer id, String clave, String nombre) {
+        this.id = id;
+        this.clave = clave;
+        this.nombre = nombre;
+    }
 }
