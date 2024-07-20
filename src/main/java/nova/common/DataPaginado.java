5e9bf5e2bf0dc4796deb4dcc4911cec0380b4425
Pablo Reyes <p.reyes1704@gmail.com>
2024-07-20 03:49:32 -0600

    Se agrega soporte constructor

--- src/main/java/nova/common/DataPaginado.java
+++ src/main/java/nova/common/DataPaginado.java
@@ -1,15 +1,20 @@
 package nova.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class DataPaginado<T> {
     private List<T> data;
     private Paginador paginador;
+
 }
