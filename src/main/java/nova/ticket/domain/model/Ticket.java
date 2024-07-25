4280539185f782f284e28fe8fee97c8b88b75013
Pablo Reyes <p.reyes1704@gmail.com>
2024-07-25 09:45:19 -0600

    Se agrega soporte validator
    Se definen tags para mensajes de validación. Soporte de constructores

--- src/main/java/nova/ticket/domain/model/Ticket.java
+++ src/main/java/nova/ticket/domain/model/Ticket.java
@@ -1,7 +1,10 @@
 package nova.ticket.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import nova.area.domain.model.Area;
 import nova.area.domain.model.Reporte;
 import nova.estado.domain.model.Estado;
@@ -9,16 +12,21 @@ import nova.unidad.domain.model.Unidad;
 
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Ticket {
     private Integer id;
+    @NotNull(message = "No se puede procesar el ticket sin un reporte asociado. Por favor, asegúrate de incluir un reporte válido.")
     private Reporte reporte;
     private Area area;
     private String folio;
+    @NotNull(message = "La unidad es obligatoria. Por favor, asegúrate de incluir una unidad válida.")
     private Unidad unidad;
     private Estado estado;
     private String agente;
     private String nota;
     private LocalDateTime fecha;
+    private Boolean visible;
 }
