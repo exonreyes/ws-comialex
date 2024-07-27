a3f9409ba29707cdabc41e3b693b5b80d463b30c
Pablo Reyes <p.reyes1704@gmail.com>
2024-07-27 08:15:06 -0600

    Se agrega soporte para medios no soportados

--- src/main/java/nova/common/exception/ExceptionManager.java
+++ src/main/java/nova/common/exception/ExceptionManager.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -49,5 +50,10 @@ public class ExceptionManager {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
                 .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<NovaResponse> HttpNoSoportado(HttpMediaTypeNotSupportedException ex) {
+        return new ResponseEntity<>(NovaResponse.builder().error("Tipo de medio no soportado").message("El tipo de contenido 'application/x-www-form-urlencoded;charset=UTF-8' no es soportado.").status(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()).build(), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 }
